89324d13f09cd2aa96cf6e7bb718300911e8b5a9
Kestutis Januskevicius <infohata@gmail.com>
2022-07-11 12:05:27 +0300

    Post ListCreate

--- postit/postit_posts/serializers.py
+++ postit/postit_posts/serializers.py
@@ -3,6 +3,10 @@ from . import models
 
 
 class PostSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    user_id = serializers.ReadOnlyField(source='user.id')
+
     class Meta:
         model = models.Post
-        fields = ('id', 'title', 'content', 'user', 'created_at', 'updated_at', )
+        fields = ('id', 'title', 'content', 'user', 'user_id', 'created_at', 'updated_at', )
+
