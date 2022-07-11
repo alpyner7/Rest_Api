8f708c772503bc7e1481aa4fabfda8214243fa6d
Kestutis Januskevicius <infohata@gmail.com>
2022-07-11 10:11:20 +0300

    initial models

--- postit/postit_posts/models.py
+++ postit/postit_posts/models.py
@@ -1,3 +1,65 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(_("title"), max_length=150)
+    content = models.TextField(_("content"))
+    user = models.ForeignKey(
+        get_user_model(), 
+        verbose_name=_("user"), 
+        on_delete=models.CASCADE,
+        related_name='posts',
+    )
+    created_at = models.DateTimeField(_("created at"), auto_now_add=True)
+    updated_at = models.DateTimeField(_("updated at"), auto_now=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, 
+        verbose_name=_("post"), 
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+    content = models.TextField(_("content"))
+    user = models.ForeignKey(
+        get_user_model(), 
+        verbose_name=_("user"), 
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+    created_at = models.DateTimeField(_("created at"), auto_now_add=True)
+    updated_at = models.DateTimeField(_("updated at"), auto_now=True)
+
+
+class PostLike(models.Model):
+    post = models.ForeignKey(
+        Post, 
+        verbose_name=_("post"), 
+        on_delete=models.CASCADE,
+        related_name='likes',
+    )
+    user = models.ForeignKey(
+        get_user_model(), 
+        verbose_name=_("user"), 
+        on_delete=models.SET_NULL,
+        related_name='post_likes',
+        null=True, blank=True,
+    )
+
+class CommentLike(models.Model):
+    comment = models.ForeignKey(
+        Comment, 
+        verbose_name=_("comment"), 
+        on_delete=models.CASCADE,
+        related_name='likes',
+    )
+    user = models.ForeignKey(
+        get_user_model(), 
+        verbose_name=_("user"), 
+        on_delete=models.SET_NULL,
+        related_name='comment_likes',
+        null=True, blank=True,
+    )
